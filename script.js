cc94113706cd6c20bfe112b6d20acc35849eca89
Ben Capp <bentucker333@gmail.com>
2023-01-21 11:20:21 -0600

    created state update for adding employee

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-console.log("script loaded");
-
+// initialize jquery
 $(document).ready(onReady);
 
+// declare state variables
+let employees = [];
+
 function onReady() {
-  console.log("jquery loaded");
+  //EVENTS: event listeners
+  $(document).on("click", "#submit-btn", onSubmit);
+
+  render();
 }
+
+// creates a new employee object and renders the page
+function onSubmit(evt) {
+  // prevent default behavior of submit button
+  evt.preventDefault();
+  // access input values and place them in a new object
+  const newEmployee = {
+    firstName: $("#first-input").val(),
+    lastName: $("#last-input").val(),
+    employeeID: $("#ID-input").val(),
+    jobTitle: $("#job-input").val(),
+    annualSalary: $("#salary-input").val(),
+  };
+
+  console.log(newEmployee);
+  // push that object to the employees array
+}
+
+// render state changes to the DOM
+function render() {}
